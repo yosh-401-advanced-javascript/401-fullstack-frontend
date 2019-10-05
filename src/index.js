e2dbfa025bd87e1c7ef97f90cb087fa60ad6ddc7
lilbonito <sauteboy@gmail.com>
2019-10-04 17:29:56 -0700

    index moved... again

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import createStore from './src/store/createStore';
-import App from './src/app';
+import createStore from './store/createStore';
+import App from './app';
 
 const store = createStore();
 
